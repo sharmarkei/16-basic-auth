264b73f5dc8344803f340f9696fe42a87d4e9c9b
sharmarke <smarky292@gmail.com>
2017-08-07 16:06:31 -0700

    setup user model

--- lab-sharmarke/model/user.js
+++ lab-sharmarke/model/user.js
@@ -28,3 +28,47 @@ userSchema.methods.generatePasswordHash = function(password) {
     });
   });
 }
+
+userSchema.methods.comparePasswordHash = function(password) {
+  debug('comparePassWordHash');
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, this.password, (err, valid) =>{
+      if (err) return reject(err);
+      if (!valid) return reject(createError(401, 'invalid password'));
+      resolve(this);
+    });
+  });
+}
+
+userSchema.methods.generateFindHash = function() {
+  debug('generateFindHash');
+
+  return new Promise((resolve, reject) => {
+    let tries = 0;
+    
+    _generateFindHash.call(this);
+
+    function _generateFindHash() {
+      this.findHash = crypto.randomBytes(32).toString('hex');
+      this.save()
+      .then( () => resolve(this.findHash))
+      .catch( err => {
+        if (tries > 3) return reject(err);
+        tries++;
+        _generateFindHash.call(this);
+      });
+    }
+  });
+}
+
+userSchema.methods.generateToken = function() {
+  debug('generateToken');
+
+  return new Promise((resolve, reject) => {
+    this.generateFindHash()
+    .then( findHash => resolve(jwt.sign({token: findHash}, process.env.APP_SECRET)))
+    .catch( err => reject(err));
+  });
+}
+
+module.exports = mongoose.model('user', userSchema);
